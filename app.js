51601c32fa01c2a553b8fe6bbe18cdb36f157a3b
Atomic82 <tom@atomic82.com>
2022-02-21 14:31:31 -0500

    Make RPS game

---
+++ app.js
@@ -0,0 +1,56 @@
+/*-------------------------------- Constants --------------------------------*/
+const choices = ["lapis", "papyrus", "scalpellus"]
+
+
+/*-------------------------------- Variables --------------------------------*/
+let playerChoice, compChoice, msg
+
+
+/*------------------------ Cached Element References ------------------------*/
+const result = document.getElementById("result-display")
+
+
+/*----------------------------- Event Listeners -----------------------------*/
+document.querySelectorAll("button").forEach(function (button) {
+  button.addEventListener("click", play)
+})
+
+
+/*-------------------------------- Functions --------------------------------*/
+function getPlayerChoice(event) {
+  playerChoice = event.target.id
+}
+
+function play(event) {
+  getPlayerChoice(event)
+}
+
+function getComputerChoice() {
+  const randomIndex = Math.floor(Math.random() * choices.length)
+  compChoice = choices[randomIndex]
+}
+
+function compare() {
+  if (playerChoice === compChoice) {
+    msg = 'You tied!'
+  } else if (playerChoice === choices[0] && compChoice === choices[2]) {
+    msg = "Congrats! You win!"
+  } else if (playerChoice === choices[1] && compChoice === choices[0]) {
+    msg = "Congrats! You win!"
+  } else if (playerChoice === choices[2] && compChoice === choices[1]) {
+    msg = "Congrats! You win!"
+  } else {
+    msg = "You lose! Try again?"
+  }
+}
+
+function render() {
+  result.innerText = `You chose ${playerChoice} and the computer chose ${compChoice}. ${msg}`
+}
+
+function play(event) {
+  getPlayerChoice(event)
+  getComputerChoice()
+  compare()
+  render()
+}
